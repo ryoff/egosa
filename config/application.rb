588d479c3c897cc16a55c1a36981d864f9105ba1
ryoff <ryoffes@gmail.com>
2017-05-08 17:19:47 +0900

    Set config.generators

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,15 @@ module Egosa
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.jbuilder false
+      g.test_framework :rspec, routing_specs: false, request_specs: false, controller_specs: false, view_specs: false, helper_specs: false, fixture: true
+      g.integration_tool false
+      g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
 end
