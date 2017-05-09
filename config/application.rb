3f7d8f87cd69022ca6c1c7e0c82fce4420ce9f05
ryoff <ryoffes@gmail.com>
2017-05-09 16:05:50 +0900

    Set time_zone

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Egosa
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.time_zone = 'Tokyo'
+
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
